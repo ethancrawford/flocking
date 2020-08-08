baa1a82f7782fa2119e4c142d60adc48c13d3226
Ethan Crawford <ethan_jc@hotmail.com>
2020-08-08 18:54:06 +0800

    Attempt to add multiple frequencies into the UGen
    NOTE: This currently causes scsynth to crash!
    Something in this change is not right, but I am yet to figure it out.

--- Flock/Flock.cpp
+++ Flock/Flock.cpp
@@ -38,6 +38,7 @@ public:
   Flock() {
     CTOR_GET_BUF;
     m_seed = IN0(3);
+    m_num_boids = IN0(4);
     rand_buf = bufData;
     rand_buf_size = bufSamples;
     for (int i = 0; i < 20; i++) {
@@ -50,7 +51,6 @@ public:
 
     // 1. set the calculation function.
     set_calc_function<Flock,&Flock::next_k>();
-    m_phase = (int32)(m_phasein * m_radtoinc);
 
     // 2. initialize the unit generator state variables.
     // initialize a constant for multiplying the frequency
@@ -58,10 +58,24 @@ public:
     int tableSize2 = ft->mSineSize;
     m_radtoinc = tableSize2 * (rtwopi * 65536.0);
     m_cpstoinc = tableSize2 * SAMPLEDUR * 65536.0;
+    m_phase = (int32*)RTAlloc(unit->mWorld, m_num_boids * sizeof(int32));
+    phase = (int32*)RTAlloc(unit->mWorld, m_num_boids * sizeof(int32));
+    sin_val = (float*)RTAlloc(unit->mWorld, m_num_boids * sizeof(float));
+    phaseinc = (int32*)RTAlloc(unit->mWorld, m_num_boids * sizeof(int32));
+    for(int i = 0; i < m_num_boids; i++){
+      m_phase[i] = (int32)(m_phasein * m_radtoinc);
+    }
     m_lomask = (tableSize2 - 1) << 3;
 
     // 3. calculate one sample of output.
     next_k(1);
+  }
+
+  ~Flock() {
+    RTFree(unit->mWorld, m_phase);
+    RTFree(unit->mWorld, phase);
+    RTFree(unit->mWorld, sin_val);
+    RTFree(unit->mWorld, phaseinc);
   }
 
 private:
@@ -70,15 +84,19 @@ private:
   const float WIDTH = 1000.0;
   const float HEIGHT = 1000.0;
   BoidFlock flock;
-  int32 m_phase;
+  int32 *m_phase;
   float m_phasein;
   double m_radtoinc;
   double m_cpstoinc;
   int32 m_lomask;
   int m_seed;
+  int m_num_boids;
   float *rand_buf;
   int rand_buf_size;
   const Unit* unit = this;
+  int32 *phase;
+  float *sin_val;
+  int32 *phaseinc;
   //////////////////////////////////////////////////////////////////
 
   // The calculation function executes once per control period
@@ -102,23 +120,28 @@ private:
     // get phase from struct and store it in a local variable.
     // The optimizer will cause it to be loaded it into a register.
     float phasein = IN0(2);
-    int32 phase = m_phase;
     int32 lomask = m_lomask;
 
     flock.flocking();
+    float out_val = 0.0;
 
-    // This works fine for manipulating a waveform containing a single frequency
-    // I don't know how to properly add multiple frequencies in and manipulate those too
-    int32 freq = (int32)(m_cpstoinc * (freqin + (flock.getBoid(0).location.x / 10.0)));
-    int32 phaseinc = freq + (int32)(CALCSLOPE(phasein, m_phasein) * m_radtoinc);
-    m_phasein = phasein;
+    for(int i = 0; i < m_num_boids; i++){
+      phase[i] = m_phase[i];
 
-    for (int i = 0; i < inNumSamples; ++i) {
+      int32 temp_freq = (int32)(m_cpstoinc * (freqin + (flock.getBoid(i).location.x / 10.0)));
+      phaseinc[i] = temp_freq + (int32)(CALCSLOPE(phasein, m_phasein) * m_radtoinc);
+      m_phasein = phasein;
 
-      outBuf[i] = lookupi1(table0, table1, phase, lomask);
-      phase += phaseinc;
+      for (int j = 0; j < inNumSamples; ++j) {
+        sin_val[i] = lookupi1(table0, table1, phase[i], lomask);
+        out_val += (sin_val[i] * 0.05);
+        phase[i] += phaseinc[i];
+      }
+      m_phase[i] = phase[i];
     }
-    m_phase = phase;
+    for(int i = 0; i < inNumSamples; ++i){
+      outBuf[i] = out_val;
+    }
   }
 
   float buf_rand(int idx) {
--- Flock/Flock.sc
+++ Flock/Flock.sc
@@ -1,9 +1,9 @@
 // without mul and add.
 Flock : UGen {
-    *ar { arg bufnum = 0, freq = 440.0, iphase = 0.0, seed = 0;
-        ^this.multiNew('audio', bufnum, freq, iphase, seed)
+    *ar { arg bufnum = 0, freq = 440.0, iphase = 0.0, seed = 0, num_boids = 20;
+        ^this.multiNew('audio', bufnum, freq, iphase, seed, num_boids)
     }
-    *kr { arg bufnum = 0, freq = 440.0, iphase = 0.0, seed = 0;
-        ^this.multiNew('control', bufnum, freq, iphase, seed)
+    *kr { arg bufnum = 0, freq = 440.0, iphase = 0.0, seed = 0, num_boids = 20;
+        ^this.multiNew('control', bufnum, freq, iphase, seed, num_boids)
     }
 }
